8e3ac800718ddd4c843a53d1353f515c18d6a074
MarckRDA <marcos.edo132@gmail.com>
2020-10-02 08:59:58 -0300

    Code refactored

--- Test.Exercise6/Candidate.cs
+++ Test.Exercise6/Candidate.cs
@@ -5,7 +5,7 @@ namespace Test.Exercise6
     public class Candidate
     {
         public Guid Id {get; private set;}      
-        public string Cpf {get; set;} 
+        public string Cpf {get; private set;} 
         public string Name { get; set; }
         public int Vote { get; set; }
 
@@ -14,6 +14,7 @@ namespace Test.Exercise6
             Id = Guid.NewGuid();
             this.Name = name;
             this.Cpf = cpf;
+            this.Vote = 0;
         }
     }
 }
